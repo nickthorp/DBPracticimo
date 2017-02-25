caf56a20c2b99cd95420841bbb08e445b4dc4e8d
Nicholas Thorp <nickthorp20@gmail.com>
2017-02-25 01:19:54 -0600

    Initial commit

---
+++ src/main/java/Book.java
@@ -0,0 +1,7 @@
+/**
+ * Created by Nicholas on 8/12/2016.
+ */
+public class Book {
+
+
+}
---
+++ src/main/java/DBPractice.java
@@ -0,0 +1,66 @@
+import java.sql.*;
+
+public class DBPractice {
+
+    private static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    private static final String DB_URL = "jdbc:mysql://192.168.0.5:3306/HOARD";
+
+    private static final String USER = "nthorp";
+    private static final String PASS = "nopat";
+
+    public static void main(String[] args){
+        Connection conn = null;
+        Statement stmt = null;
+        try{
+            Class.forName(JDBC_DRIVER);
+
+            System.out.println("Connecting to a database...");
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+            System.out.println("Creating a statement...");
+            stmt = conn.createStatement();
+            String sql;
+            sql = "SELECT * FROM hoard_user";
+            ResultSet rs = stmt.executeQuery(sql);
+
+            //STEP 5: Extract data from result set
+            while(rs.next()){
+                //Retrieve by column name
+                String user_email  = rs.getString("user_email");
+                String user_username = rs.getString("user_username");
+                String user_first_name = rs.getString("user_first_name");
+                String user_last_name = rs.getString("user_last_name");
+
+                //Display values
+                System.out.print("Email: " + user_email);
+                System.out.print(", Username: " + user_username);
+                System.out.print(", First: " + user_first_name);
+                System.out.println(", Last: " + user_last_name);
+            }
+            //STEP 6: Clean-up environment
+            rs.close();
+            stmt.close();
+            conn.close();
+        }catch(SQLException se){
+            //Handle errors for JDBC
+            se.printStackTrace();
+        }catch(Exception e){
+            //Handle errors for Class.forName
+            e.printStackTrace();
+        }finally {
+            //finally block used to close resources
+            try {
+                if (stmt != null)
+                    stmt.close();
+            } catch (SQLException se2) {
+            }// nothing we can do
+            try {
+                if (conn != null)
+                    conn.close();
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }//end finally try
+        }
+        System.out.println("Peace!");
+    }
+}
